f2c630f7fb39d323d79f853fe8b6757d54e6b635
joeyu <joewah.yu@seattlecolleges.edu>
2025-05-21 21:23:03 -0700

    Added code comments.

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # Home URL, the HTML page displayed when website first loads
 def home():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/about') # About page, accessed by link or by adding "/about" to home URL
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact') # Contact page, accessed by link or by adding "/contact" to home URL
 def contact():
     return render_template('contact.html')
 
